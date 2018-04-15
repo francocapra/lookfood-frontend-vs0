cfd2720bab650e9791cb99b864f7dc7b23e13a35
Guilherme <guilherme@Guilhermes-MacBook-Pro.local>
2018-04-14 22:32:39 -0300

    Adjusted review list

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 sap.ui.getCore().attachInit(function () {
 
-    console.log(JSON.stringify(getSamplePartnerCollection()));
-
     $.fn.removeClassStartingWith = function (filter) {
         $(this).removeClass(function (index, className) {
             return (className.match(new RegExp("\\S*" + filter + "\\S*", 'g')) || []).join(' ')
@@ -27,10 +25,10 @@ sap.ui.getCore().attachInit(function () {
                     let reviewCode = null;
 
                     var _previewDialog = new sap.m.Dialog({
-                        title: 'Posicione o QR Code em frente o leitor',
+                        title: 'Posicione o código em frente o leitor',
                         content: [
                             new sap.ui.core.HTML({
-                                content: '<video id="preview" width="200" heigth="200"></video>'
+                                content: '<video id="preview"></video>'
                             })
                         ],
                         beginButton: new sap.m.Button({
@@ -46,7 +44,7 @@ sap.ui.getCore().attachInit(function () {
                                 _previewDialog.close();
                             });
                             Instascan.Camera.getCameras().then(function (cameras) {
-                                alert(cameras[0].name)
+                                // alert(cameras[0].name)
                                 if (cameras.length > 0) {
                                     scanner.activeCameraId = cameras[0].id;
                                     scanner.start(cameras[0]);
@@ -66,6 +64,7 @@ sap.ui.getCore().attachInit(function () {
                         afterClose: function () {
                             _previewDialog.destroy();
                             console.log(reviewCode);
+                            inflateReviews(getSamplePartnerCollection());
                             // sap.ui.core.BusyIndicator.show(0);
 
                             // $.ajax({
@@ -83,7 +82,7 @@ sap.ui.getCore().attachInit(function () {
                             //     }
                             // })
                         }
-                    }).open();
+                    }).addStyleClass('preview-dialog').open();
 
                 }
             })
@@ -94,26 +93,34 @@ sap.ui.getCore().attachInit(function () {
 
 function inflateReviews(oData) {
 
-    var oFoodsModel = new sap.ui.model.json.JSONModel(oData);
-    sap.ui.getCore().setModel(oFoodsModel, 'foodmodel');
+    var oPartnerCollection = new sap.ui.model.json.JSONModel(oData);
+    sap.ui.getCore().setModel(oPartnerCollection, 'partnermodel');
 
-    var foodList = new sap.m.List({
-        headerText: "Consumo"
+    var productList = new sap.m.List({
+        headerText: "List de Produtos"
     });
 
-    foodList.bindItems({
-        path: 'foodmodel>/FoodCollection',
+    productList.bindItems({
+        path: 'partnermodel>/PartnerCollection/ProductCollection',
         template: new sap.m.ObjectListItem({
-            title: '{foodmodel>foodName}',
-            intro: '{foodmodel>foodChef}',
-            number: '{foodmodel>foodPrice}',
+            title: '{partnermodel>ProductName}',
+            // intro: '{partnermodel>ProductId}',
+            // number: '{partnermodel>foodPrice}',
             icon: 'sap-icon://meal',
             numberUnit: 'R$',
             type: 'Active',
+            attributes: [
+                new sap.m.ObjectAttribute({
+                    text: '{partnermodel>ResponsibleName}'
+                }),
+                new sap.m.ObjectAttribute({
+                    text: '{partnermodel>AuxiliaryName}'
+                })
+            ],
             tap: function (evt) {
 
-                var _customerId = this.data('customerId');
-                var _foodId = this.data('foodId');
+                var _partnerId = this.data('partnerId');
+                var _productId = this.data('productId');
 
                 var _dialog = new sap.m.Dialog({
                     title: 'Informe a nota da avaliação',
@@ -177,9 +184,9 @@ function inflateReviews(oData) {
 
                                         var _reviewValue = sap.ui.getCore().byId('reviewSlider').getValue();
 
-                                        var foodReview = {
-                                            customerId: _customerId,
-                                            foodId: _foodId,
+                                        var partnerReview = {
+                                            partnerId: _partnerId,
+                                            productId: _productId,
                                             reviewDate: String.prototype.concat(_date.getFullYear(), _month, _day),
                                             reviewTime: String.prototype.concat(_hour, _minutes, _seconds),
                                             reviewValue: _reviewValue
@@ -191,7 +198,7 @@ function inflateReviews(oData) {
                                             type: 'POST',
                                             url: 'http://' + location.hostname + ':8081/LookFood/rest/ReviewServices/SaveReview/',
                                             contentType: 'application/json',
-                                            data: JSON.stringify(foodReview),
+                                            data: JSON.stringify(partnerReview),
                                             success: function () {
                                                 _confirmationDialog.close();
                                                 sap.ui.core.BusyIndicator.hide();
@@ -234,15 +241,162 @@ function inflateReviews(oData) {
 
                 _dialog.open();
             }
-        }).data('foodId', '{foodmodel>foodId}')
-            .data('customerId', '{foodmodel>customerId}')
+        }).data('productId', '{partnermodel>ProductId}')
+            .data('partnerId', '{partnermodel>PartnerId}')
+    })
+
+    var serviceList = new sap.m.List({
+        headerText: "List de Serviços"
+    });
+
+    serviceList.bindItems({
+        path: 'partnermodel>/PartnerCollection/ServiceCollection',
+        template: new sap.m.ObjectListItem({
+            title: '{partnermodel>ServiceName}',
+            // intro: '{partnermodel>ServiceId}',
+            // number: '{partnermodel>foodPrice}',
+            icon: 'sap-icon://employee',
+            numberUnit: 'R$',
+            type: 'Active',
+            attributes:[
+                new sap.m.ObjectAttribute({
+                    text:'{partnermodel>ResponsibleName}'
+                })
+            ],
+            tap: function (evt) {
+
+                var _partnerId = this.data('partnerId');
+                var _serviceId = this.data('serviceId');
+
+                var _dialog = new sap.m.Dialog({
+                    title: 'Informe a nota da avaliação',
+                    contentWidth: '600px',
+                    content: [
+                        new sap.ui.layout.VerticalLayout({
+                            content: [
+                                new sap.m.Text('reviewValue', {
+                                    text: '0'
+                                }).addStyleClass('review-value-low'),
+                                new sap.m.Slider('reviewSlider', {
+                                    width: '80%',
+                                    min: 0,
+                                    max: 10,
+                                    step: 1,
+                                    liveChange: function (evt, a, b) {
+                                        var _value = evt.getSource().getValue();
+                                        var _label = $('#reviewValue');
+
+                                        if (_value >= 0 & _value <= 3) {
+                                            _label.removeClassStartingWith('review-');
+                                            _label.addClass('review-value-low');
+                                        }
+                                        else if (_value > 3 & _value <= 7) {
+                                            _label.removeClassStartingWith('review-');
+                                            _label.addClass('review-value-medium');
+                                        }
+                                        else if (_value > 7 & _value <= 10) {
+                                            _label.removeClassStartingWith('review-');
+                                            _label.addClass('review-value-high');
+                                        }
+
+                                        _label.text(_value);
+                                    }
+                                })
+                            ]
+                        }).addStyleClass('review_dialog_note_layout')
+                    ],
+                    beginButton: new sap.m.Button({
+                        text: 'Confirmar',
+                        type: sap.m.ButtonType.Emphasized,
+                        press: function () {
+                            var _confirmationDialog = new sap.m.Dialog({
+                                title: 'Confirmação',
+                                type: 'Message',
+                                content: [
+                                    new sap.m.Text({
+                                        text: 'Confirma review para o serviço?'
+                                    })
+                                ],
+                                beginButton: new sap.m.Button({
+                                    text: 'Sim',
+                                    press: function () {
+
+                                        var _date = new Date();
+                                        var _month = (_date.getMonth() + 1) < 10 ? ('0' + (_date.getMonth() + 1)) : (_date.getMonth() + 1);
+                                        var _day = (_date.getDate() + 1) < 10 ? ('0' + _date.getDate()) : _date.getDate();
+                                        var _hour = _date.getHours() < 10 ? ('0' + _date.getHours()) : _date.getHours();
+                                        var _minutes = _date.getMinutes() < 10 ? ('0' + _date.getMinutes()) : _date.getMinutes();
+                                        var _seconds = _date.getSeconds() < 10 ? ('0' + _date.getSeconds()) : _date.getSeconds();
+
+                                        var _reviewValue = sap.ui.getCore().byId('reviewSlider').getValue();
+
+                                        var serviceReview = {
+                                            partnerId: _partnerId,
+                                            serviceId: _serviceId,
+                                            reviewDate: String.prototype.concat(_date.getFullYear(), _month, _day),
+                                            reviewTime: String.prototype.concat(_hour, _minutes, _seconds),
+                                            reviewValue: _reviewValue
+                                        }
+
+                                        sap.ui.core.BusyIndicator.show(0);
+
+                                        $.ajax({
+                                            type: 'POST',
+                                            url: 'http://' + location.hostname + ':8081/LookFood/rest/ReviewServices/SaveReview/',
+                                            contentType: 'application/json',
+                                            data: JSON.stringify(serviceReview),
+                                            success: function () {
+                                                _confirmationDialog.close();
+                                                sap.ui.core.BusyIndicator.hide();
+
+                                                sap.m.MessageToast.show('Voto computado com sucesso.\nObrigado!', {
+                                                    duration: 3000
+                                                })
+                                            },
+                                            error: function (a, b, c) {
+                                                console.log(a, b, c);
+                                                _confirmationDialog.close();
+                                                sap.ui.core.BusyIndicator.hide();
+                                            }
+                                        })
+                                    }
+                                }),
+                                endButton: new sap.m.Button({
+                                    text: 'Cancelar',
+                                    press: function () {
+                                        _confirmationDialog.close();
+                                    }
+                                }),
+                                afterClose: function () {
+                                    _dialog.close();
+                                    _confirmationDialog.destroy();
+                                }
+                            }).open();
+                        }
+                    }),
+                    endButton: new sap.m.Button({
+                        text: 'Fechar',
+                        press: function () {
+                            _dialog.close();
+                        }
+                    }),
+                    afterClose: function () {
+                        _dialog.destroy();
+                    }
+                });
+
+                _dialog.open();
+            }
+        }).data('serviceId', '{partnermodel>ServiceId}')
+            .data('partnerId', '{partnermodel>PartnerId}')
     })
 
     var _reviewDialog = new sap.m.Dialog({
         title: 'Avaliação de estabelecimento',
         minWidth: '100%',
         content: [
-            foodList
+            productList,
+            serviceList
         ],
         beginButton: new sap.m.Button({
             text: 'Close',
--- js/tdc.js
+++ js/tdc.js
@@ -4,36 +4,54 @@ function getSamplePartnerCollection() {
         PartnerCollection: {
             ProductCollection: [
                 {
-                    PartnerId: 'LFC0000001',
+                    PartnerId: 'LFPRT00001',
                     ProductId: 'LFPRD00001',
-                    ProductName: 'Product 1'
+                    ProductName: 'Product 1',
+                    ResponsibleId:'LFPER00001',
+                    ResponsibleName:'Franco Capra',
+                    AuxiliaryId:'LFPER00002',
+                    AuxiliaryName:'Guilherme Giani'
                 },
                 {
-                    PartnerId: 'LFC0000001',
+                    PartnerId: 'LFPRT00001',
                     ProductId: 'LFPRD00002',
-                    ProductName: 'Product 2'
+                    ProductName: 'Product 2',
+                    ResponsibleId:'LFPER00001',
+                    ResponsibleName:'Franco Capra',
+                    AuxiliaryId:'LFPER00002',
+                    AuxiliaryName:'Guilherme Giani'
                 },
                 {
-                    PartnerId: 'LFC0000001',
+                    PartnerId: 'LFPRT00001',
                     ProductId: 'LFPRD00003',
-                    ProductName: 'Product 3'
+                    ProductName: 'Product 3',
+                    ResponsibleId:'LFPER00001',
+                    ResponsibleName:'Franco Capra',
+                    AuxiliaryId:'LFPER00002',
+                    AuxiliaryName:'Guilherme Giani'
                 }
             ],
             ServiceCollection: [
                 {
-                    PartnerId: 'LFC0000001',
+                    PartnerId: 'LFPRT00001',
                     ServiceId: 'LFSVC00001',
-                    ServiceName: 'Service 1'
+                    ServiceName: 'Service 1',
+                    ResponsibleId:'LFPER00001',
+                    ResponsibleName:'Guilherme Giani'
                 },
                 {
-                    PartnerId: 'LFC0000001',
+                    PartnerId: 'LFPRT00001',
                     ServiceId: 'LFSVC00002',
-                    ServiceName: 'Service 2'
+                    ServiceName: 'Service 2',
+                    ResponsibleId:'LFPER00001',
+                    ResponsibleName:'Guilherme Giani'
                 },
                 {
-                    PartnerId: 'LFC0000001',
-                    ServiceId: 'LFSVC00002',
-                    ServiceName: 'Service 3'
+                    PartnerId: 'LFPRT00001',
+                    ServiceId: 'LFSVC00003',
+                    ServiceName: 'Service 3',
+                    ResponsibleId:'LFPER00001',
+                    ResponsibleName:'Guilherme Giani'
                 }
             ]
         }
