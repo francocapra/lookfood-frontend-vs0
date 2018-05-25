b505b03b3f83c62270cd3d734aab609d60620315
Guilherme Giani <guilhermegiani@Guilhermes-MacBook-Pro.local>
2018-05-25 16:19:04 -0300

    product details

--- controllers/ItemManagement.controller.js
+++ controllers/ItemManagement.controller.js
@@ -51,76 +51,23 @@ sap.ui.define([
 				});
 			},
 
-			onSelectionChange: function(oEvent) {
-				let oSelectedItem = oEvent.getParameter("listItem");
-				let oModel = oSelectedItem.getBindingContext('PartnerPrdCollection').getObject();
-				
-				let oDialog = new sap.m.Dialog({
-					title:oBaseController.getResourceBundle().getText('itemDialogHeader'),
-					content:[
-					new sap.m.HBox({
-						items:[
-						new sap.m.FlexBox({
-							alignItems:'Start',
-							justifyContent:'Center',
-							items:[
-							new sap.m.Image({
-								src:'imgs/food.png',
-								width:'128px',
-								height:'128px'
-							}).addStyleClass('productPicture')
-							]
-						}),
-						new sap.m.VBox({
-							items:[
-							new sap.m.Label({
-								text: oBaseController.getResourceBundle().getText('tableColumnItem')
-							}),
-							new sap.m.Text({
-								text: oModel.id
-							}).addStyleClass('sapUiSmallMarginBottom'),
-							new sap.m.Label({
-								text:oBaseController.getResourceBundle().getText('tableColumnDesc'),
-								labelFor:'txtItemDesc'
-							}),
-							new sap.m.Input('txtItemDesc',{
-								value:oModel.description
-							}),
-							new sap.m.Label({
-								text: oBaseController.getResourceBundle().getText('tableColumnChef'),
-								labelFor:'txtItemChef'
-							}),
-							new sap.m.Input('txtItemChef',{
-								value: oModel.chef
-							}),
-							new sap.m.Label({
-								text: oBaseController.getResourceBundle().getText('tableColumnAux'),
-								labelFor:'txtItemAux'
-							}),
-							new sap.m.Input('txtItemAux',{
-								value: oModel.aux
-							})
-							]
-						})
-						]
-					}).addStyleClass('sapUiTinyMargin sapUiMediumMarginEnd')
-					],
-					beginButton:new sap.m.Button({
-						type:'Emphasized',
-						text: oBaseController.getResourceBundle().getText('btnSave')
-					}),
-					endButton:new sap.m.Button({
-						text: oBaseController.getResourceBundle().getText('btnCancel'),
-						press:function(){
-							oDialog.close();
-						}
-					}),
-					afterClose:function(){
-						oDialog.destroy();
-					}
-				});
+			onListItemPress: function(oEvent) {
 
-				oDialog.open();
+				let oModel = oEvent.getSource().getBindingContext('PartnerPrdCollection').getObject();
+				
+				let prdDetails = sap.ui.xmlfragment('gourmeo.xml.fragments.ProductDetails', this);
+
+				prdDetails.setModel(new sap.ui.model.json.JSONModel(oModel), 'mProductDetails')
+
+				this.getView().addDependent(prdDetails);
+
+				prdDetails.attachAfterClose(function(dialogCloseEvent){
+					dialogCloseEvent.getSource().destroy();
+				}).open();
+			},
+
+			closeDialog:function(oEvent){
+				oEvent.getSource().getParent().close();
 			}
 		});
 
