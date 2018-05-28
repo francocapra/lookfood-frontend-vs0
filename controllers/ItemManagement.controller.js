ed83c3f0e7e1d22e9561e82625b7c0df3aede416
Guilherme Giani <guilhermegiani@Guilhermes-MacBook-Pro.local>
2018-05-27 22:14:11 -0300

    Started to implement call to upload product picture

--- controllers/ItemManagement.controller.js
+++ controllers/ItemManagement.controller.js
@@ -30,6 +30,25 @@ sap.ui.define([
 				});
 			},
 
+			uploadProductPicture:function(file, authToken){
+				let formData = new FormData();
+
+				formData.append('file', file, file.name);
+
+				return $.ajax({
+					type:'POST',
+					url:oBaseController.getServiceApi()+'products/picture',
+					data: formData,
+					async:true,
+					cache:false,
+					contentType:false,
+					processData:false,
+					beforeSend:function(request){
+						request.setRequestHeader('Authorization', authToken)
+					}
+				});
+			},
+
 			onRefreshItemsPress: function(){
 
 				oBaseController.getView().setBusyIndicatorDelay(0).setBusy(true);
@@ -55,11 +74,16 @@ sap.ui.define([
 
 				let oModel = oEvent.getSource().getBindingContext('PartnerPrdCollection').getObject();
 				
-				let prdDetails = sap.ui.xmlfragment('gourmeo.xml.fragments.ProductDetails', this);
+				let prdDetails = sap.ui.xmlfragment('prdDetailsFragment','gourmeo.xml.fragments.ProductDetails', this);
 
 				prdDetails.setModel(new sap.ui.model.json.JSONModel(oModel), 'mProductDetails')
 
 				this.getView().addDependent(prdDetails);
+
+				if(oModel.imageUrl)
+					sap.ui.core.Fragment.byId('prdDetailsFragment', 'imgProductPicture').setSrc(oModel.imageUrl);
+				else
+					sap.ui.core.Fragment.byId('prdDetailsFragment', 'imgProductPicture').setSrc('imgs/food-tray.png');
 
 				prdDetails.attachAfterClose(function(dialogCloseEvent){
 					dialogCloseEvent.getSource().destroy();
@@ -68,6 +92,37 @@ sap.ui.define([
 
 			closeDialog:function(oEvent){
 				oEvent.getSource().getParent().close();
+			},
+
+			onPrdImagePress:function(){
+				let _uploader = $('#productPicUploader');
+
+				_uploader.on('change', function(){
+					let file = this.files[0];
+					let authToken = window.sessionStorage.getItem('Authorization');
+
+					if(!file)
+						return;
+
+					let oDialog = sap.ui.core.Fragment.byId('prdDetailsFragment', 'prdDetailsDialog');
+
+					oDialog.setBusyIndicatorDelay(0).setBusy(true);
+
+					let authToken = window.sessionStorage.getItem('Authorization');
+
+					$.when(oBaseController.uploadProductPicture(file, authToken)).done(function(data,textStatus,jqXHR){
+						let location = jqXHR.getResponseHeader('location')
+
+						sap.ui.getCore().byId('partnerProfilePicture').setSrc(location);
+
+						pictureDialog.setBusy(false);
+					}).fail(function(a,b,c){
+						console.log(a,b,c);
+						pictureDialog.setBusy(false);
+					});
+				});
+
+				_uploader.trigger('click');
 			}
 		});
 
