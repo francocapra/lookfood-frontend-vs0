d9a2991016c38cb49a48e6ba0dc34317baaf5684
Guilherme Giani <guilhermegiani@Guilhermes-MacBook-Pro.local>
2018-05-30 23:03:05 -0300

    Scanner dialog complete. Logo changed

--- controllers/Review.controller.js
+++ controllers/Review.controller.js
@@ -23,6 +23,55 @@ sap.ui.define([
 					}
 				});
 			},
+
+			onStartReviewPress:function(){
+				let scanner = null;
+				let reviewCode = null;
+
+				let _previewDialog = new sap.m.Dialog({
+					title: oBaseController.getResourceBundle().getText('reviewScannerTitle'),
+					content: [
+					new sap.ui.core.HTML({
+						content: '<video id="preview" width="400" heigth="400"></video>'
+					})
+					],
+					beginButton: new sap.m.Button({
+						text: oBaseController.getResourceBundle().getText('btnClose'),
+						press: function () {
+							_previewDialog.close();
+						}
+					}),
+					afterOpen: function () {
+						scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+						scanner.addListener('scan', function (content) {
+							reviewCode = content;
+							_previewDialog.close();
+						});
+						Instascan.Camera.getCameras().then(function (cameras) {
+                                    // alert(cameras[0].name)
+                                    if (cameras.length > 0) {
+                                    	scanner.activeCameraId = cameras[0].id;
+                                    	scanner.start(cameras[0]);
+                                    } else {
+                                    	console.error('No cameras found.');
+                                    	alert("No cameras foud.")
+                                    }
+                                }).catch(function (e) {
+                                	console.error(e);
+                                	alert(e);
+                                });
+                            },
+                            beforeClose: function () {
+                            	scanner.stop();
+                            	scanner = null;
+                            },
+                            afterClose: function () {
+                            	_previewDialog.destroy();
+                            	console.log(reviewCode);
+
+                            }
+                        }).open();
+			}
 		});
 
 	});
